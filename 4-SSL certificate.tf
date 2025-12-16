41d3c73527c19a91bb15b05e605d478895772f67
Nadir <the.fire.dragon.mac@gmail.com>
2025-12-16 17:29:59 -0500

    Add files via upload

--- 4-SSL certificate.tf
+++ 4-SSL certificate.tf
@@ -5,6 +5,7 @@ resource "aws_acm_certificate" "cert" {
   domain_name               = var.domain_name
   subject_alternative_names = ["*.${var.domain_name}"]
   validation_method         = "DNS"
+  
 
   tags = {
     Name = "Cloudfront SSL Cert"
